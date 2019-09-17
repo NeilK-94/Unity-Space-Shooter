9f86ec1924ae682a8468b1a8b95e51f09579796b
NeilK-94 <neil.kyne@gmail.com>
2019-09-17 16:25:31 +0100

    first commit

---
+++ Assets/__Scripts/Player.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        //  Have an input from the user
+        var deltaX = Input.GetAxis("Horizontal");
+        var deltaY = Input.GetAxis("Vertical");
+
+        //  Calculated new position
+        var newXPos = transform.position.x + deltaX;
+        var newYPos = transform.position.y + deltaY;
+
+        //  Set the current game object to the new position
+        transform.position = new Vector2(newXPos, newYPos);
+
+    }
+}
